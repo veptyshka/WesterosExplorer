a90ac6be0016b830fccbad88743f8a440ba7a1d1
veptyshka <bricklink@inbox.lv>
2025-03-24 21:21:07 +0200

    still not adding idk

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from models import db, bcrypt
-from models.models import House, Seat, User
+from models.models import House, Seat, User, UserRole
 from routes.auth_routes import auth
 from routes.main_routes import main
 from routes.house_routes import house_bp
@@ -12,6 +12,7 @@ from utils import create_admin
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///westeros.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'rya'
 
 db.init_app(app)
@@ -38,8 +39,6 @@ app.register_blueprint(admin)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        db.session.remove()
-        create_admin()
     app.run(debug = True)
 
 with app.app_context():
@@ -48,42 +47,60 @@ with app.app_context():
     seats_exist = Seat.query.count() > 0
 
     if not houses_exist or not seats_exist:
-        print('Adding houses and castles...')
+        try:
+            print('Adding houses and castles...')
 
-        houses_data = [
-            {"name": "Stark", "lands": "The North", "words": "Winter is Coming", "emblem": "House_Stark.png", "seat": {"name": "Winterfell", "location": "x, y"}},
-            {"name": "Lannister", "lands": "The West", "words": "Hear me Roar!", "emblem": "House_Lannister.png", "seat": {"name": "Casterly Rock", "location": "x, y"}},
-            {"name": "Baratheon", "lands": "Stormlands", "words": "Ours is the Fury", "emblem": "House_Baratheon.png", "seat": {"name": "Storm's End", "location": "x, y"}},
-            {"name": "Targaryen", "lands": "Crownlands", "words": "Fire and Blood", "emblem": "House_Targaryen.png", "seat": {"name": "Dragonstone", "location": "x, y"}},
-            {"name": "Greyjoy", "lands": "Iron Islands", "words": "We Do Not Sow", "emblem": "House_Greyjoy.png", "seat": {"name": "Pyke", "location": "x, y"}},
-            {"name": "Tully", "lands": "Riverlands", "words": "Family, Duty, Honor", "emblem": "House_Tully.png", "seat": {"name": "Riverrun", "location": "x, y"}},
-            {"name": "Arryn", "lands": "Vale of Arryn", "words": "As High as Honor", "emblem": "House_Arryn.png", "seat": {"name": "Eyrie", "location": "x, y"}},
-            {"name": "Tyrell", "lands": "Reach", "words": "Growing Strong", "emblem": "House_Tyrell.png", "seat": {"name": "Highgarden", "location": "x, y"}},
-            {"name": "Martell", "lands": "Dorne", "words": "Unbowed, Unbent, Unbroken", "emblem": "House_Martell.png", "seat": {"name": "Sunspear", "location": "x, y"}},
-            {"name": "Night's Watch", "lands": "The Wall", "words": "Sword in the darkness", "emblem": "Night's_Watch.png", "seat": {"name": "Castle Black", "location": "x, y"}},
-        ]
+            houses_data = [
+                {"name": "Stark", "lands": "The North", "words": "Winter is Coming", "emblem": "House_Stark.png", "seat": {"name": "Winterfell", "location": "x, y"}},
+                {"name": "Lannister", "lands": "The West", "words": "Hear me Roar!", "emblem": "House_Lannister.png", "seat": {"name": "Casterly Rock", "location": "x, y"}},
+                {"name": "Baratheon", "lands": "Stormlands", "words": "Ours is the Fury", "emblem": "House_Baratheon.png", "seat": {"name": "Storm's End", "location": "x, y"}},
+                {"name": "Targaryen", "lands": "Crownlands", "words": "Fire and Blood", "emblem": "House_Targaryen.png", "seat": {"name": "Dragonstone", "location": "x, y"}},
+                {"name": "Greyjoy", "lands": "Iron Islands", "words": "We Do Not Sow", "emblem": "House_Greyjoy.png", "seat": {"name": "Pyke", "location": "x, y"}},
+                {"name": "Tully", "lands": "Riverlands", "words": "Family, Duty, Honor", "emblem": "House_Tully.png", "seat": {"name": "Riverrun", "location": "x, y"}},
+                {"name": "Arryn", "lands": "Vale of Arryn", "words": "As High as Honor", "emblem": "House_Arryn.png", "seat": {"name": "Eyrie", "location": "x, y"}},
+                {"name": "Tyrell", "lands": "Reach", "words": "Growing Strong", "emblem": "House_Tyrell.png", "seat": {"name": "Highgarden", "location": "x, y"}},
+                {"name": "Martell", "lands": "Dorne", "words": "Unbowed, Unbent, Unbroken", "emblem": "House_Martell.png", "seat": {"name": "Sunspear", "location": "x, y"}},
+                {"name": "Night's Watch", "lands": "The Wall", "words": "Sword in the darkness", "emblem": "Night's_Watch.png", "seat": {"name": "Castle Black", "location": "x, y"}},
+            ]
 
-        for house_data in houses_data:
-            # Checking for a house
-            house = House.query.filter_by(name = house_data["name"]).first()
-            if not house:
-                house = House(
-                    name = house_data["name"],
-                    lands = house_data["lands"],
-                    words = house_data["words"],
-                    emblem = house_data["emblem"],
-                )
-                db.session.add(house)
-                db.session.flush() # to get house id before adding it's seat
-                print(f"House {house.name} added!")
+            for house_data in houses_data:
+                # Checking for a house
+                house = House.query.filter_by(name = house_data["name"]).first()
+                if not house:
+                    house = House(
+                        name = house_data["name"],
+                        lands = house_data["lands"],
+                        words = house_data["words"],
+                        emblem = house_data["emblem"],
+                    )
+                    db.session.add(house)
+                    print(f"House {house.name} added!")
+            db.session.commit()
 
-            # Checking for a seat
-            seat_data = house_data["seat"]
-            seat = Seat.query.filter_by(name = seat_data["name"]).first()
-            if not seat:
-                seat = Seat(name = seat_data["name"], location = seat_data["location"], house_id = house.id)
-                db.session.add(seat)
-                print(f"Castle {seat.name} for house {house.name} added!")
+            for house_data in houses_data:
+                house = House.query.filter_by(name = house_data["name"]).first()
+                # Checking for a seat
+                seat_data = house_data["seat"]
+                seat = Seat.query.filter_by(name = seat_data["name"]).first()
+                if not seat:
+                    seat = Seat(
+                        name = seat_data["name"],
+                        location = seat_data["location"],
+                        house_id = house.id
+                    )
+                    db.session.add(seat)
+                    print(f"Castle {seat.name} for house {house.name} added!")
 
-        db.session.commit()
-        print("Successfully created houses and seats")
+            db.session.commit()
+            print("Successfully created houses and seats")
+
+        except Exception as e:
+            db.session.rollback()
+            print(f"Error occured: {e}")
+
+    try:
+        create_admin()
+    
+    except Exception as e:
+        db.session.rollback()
+        print(f"Error occured: {e}")
