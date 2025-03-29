b2a8e54905a48f13391976f99832c36500b77c7e
veptyshka <bricklink@inbox.lv>
2025-03-29 21:40:30 +0200

    castle coords

--- app.py
+++ app.py
@@ -48,16 +48,16 @@ if __name__ == '__main__':
                 print('Adding houses and castles...')
 
                 houses_data = [
-                    {"name": "Stark", "lands": "The North", "words": "Winter is Coming", "emblem": "House_Stark.png", "seat": {"name": "Winterfell", "location": "x, y"}},
-                    {"name": "Lannister", "lands": "The West", "words": "Hear me Roar!", "emblem": "House_Lannister.png", "seat": {"name": "Casterly Rock", "location": "x, y"}},
-                    {"name": "Baratheon", "lands": "Stormlands", "words": "Ours is the Fury", "emblem": "House_Baratheon.png", "seat": {"name": "Storm's End", "location": "x, y"}},
-                    {"name": "Targaryen", "lands": "Crownlands", "words": "Fire and Blood", "emblem": "House_Targaryen.png", "seat": {"name": "Dragonstone", "location": "x, y"}},
-                    {"name": "Greyjoy", "lands": "Iron Islands", "words": "We Do Not Sow", "emblem": "House_Greyjoy.png", "seat": {"name": "Pyke", "location": "x, y"}},
-                    {"name": "Tully", "lands": "Riverlands", "words": "Family, Duty, Honor", "emblem": "House_Tully.png", "seat": {"name": "Riverrun", "location": "x, y"}},
-                    {"name": "Arryn", "lands": "Vale of Arryn", "words": "As High as Honor", "emblem": "House_Arryn.png", "seat": {"name": "Eyrie", "location": "x, y"}},
-                    {"name": "Tyrell", "lands": "Reach", "words": "Growing Strong", "emblem": "House_Tyrell.png", "seat": {"name": "Highgarden", "location": "x, y"}},
-                    {"name": "Martell", "lands": "Dorne", "words": "Unbowed, Unbent, Unbroken", "emblem": "House_Martell.png", "seat": {"name": "Sunspear", "location": "x, y"}},
-                    {"name": "Night's Watch", "lands": "The Wall", "words": "Sword in the darkness", "emblem": "Night's_Watch.png", "seat": {"name": "Castle Black", "location": "x, y"}},
+                    {"name": "Stark", "lands": "The North", "words": "Winter is Coming", "emblem": "House_Stark.png", "seat": {"name": "Winterfell", "location": "270, 200"}},
+                    {"name": "Lannister", "lands": "The West", "words": "Hear me Roar!", "emblem": "House_Lannister.png", "seat": {"name": "Casterly Rock", "location": "150, 505"}},
+                    {"name": "Baratheon", "lands": "Stormlands", "words": "Ours is the Fury", "emblem": "House_Baratheon.png", "seat": {"name": "Storm's End", "location": "390, 606"}},
+                    {"name": "Targaryen", "lands": "Crownlands", "words": "Fire and Blood", "emblem": "House_Targaryen.png", "seat": {"name": "Dragonstone", "location": "412, 492"}},
+                    {"name": "Greyjoy", "lands": "Iron Islands", "words": "We Do Not Sow", "emblem": "House_Greyjoy.png", "seat": {"name": "Pyke", "location": "150, 420"}},
+                    {"name": "Tully", "lands": "Riverlands", "words": "Family, Duty, Honor", "emblem": "House_Tully.png", "seat": {"name": "Riverrun", "location": "245, 445"}},
+                    {"name": "Arryn", "lands": "Vale of Arryn", "words": "As High as Honor", "emblem": "House_Arryn.png", "seat": {"name": "Eyrie", "location": "355, 415"}},
+                    {"name": "Tyrell", "lands": "Reach", "words": "Growing Strong", "emblem": "House_Tyrell.png", "seat": {"name": "Highgarden", "location": "195, 625"}},
+                    {"name": "Martell", "lands": "Dorne", "words": "Unbowed, Unbent, Unbroken", "emblem": "House_Martell.png", "seat": {"name": "Sunspear", "location": "415, 735"}},
+                    {"name": "Night's Watch", "lands": "The Wall", "words": "Sword in the darkness", "emblem": "Night's_Watch.png", "seat": {"name": "Castle Black", "location": "320, 60"}},
                 ]
 
                 for house_data in houses_data:
